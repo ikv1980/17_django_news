ff9acc120afcef769ad98ee4e248f4208f1afd52
Konstantin Ivanov <ikv1980@gmail.com>
2022-09-14 08:53:18 +0300

    Update settings.py
    Delete value from EMAIL_HOST_PASSWORD

--- xsite/settings.py
+++ xsite/settings.py
@@ -123,7 +123,7 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'twkostik@gmail.com'
-EMAIL_HOST_PASSWORD = 'xehplhzkpdcctuqx'
+EMAIL_HOST_PASSWORD = 'delete'
 
 
 STATIC_URL = '/static/'
